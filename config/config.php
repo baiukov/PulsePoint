2d9a5a9efd2b70f4c0439657d1e399c480be8701
Aleksei Baiukov <Blue51@yandex.ru>
2023-04-30 19:43:54 +0200

    feat. add main

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('DELIMITER', '; ');
+define('DB_HOST', 'localhost');
+define('DB_DATABASE', 'sp');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+//eij9of3Rahreesoofi
+?>
